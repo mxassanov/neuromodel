cf873d16196bd05856fffbb28f555a05940c0a08
mxassanov <mxassanov@bk.ru>
2021-11-12 01:09:52 +0300

    deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'db/db.json'));
 const middlewares = jsonServer.defaults({ static: './dist' });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
