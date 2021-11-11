f495f11ca8621f30e515ac71fff9be2be715949e
mxassanov <mxassanov@bk.ru>
2021-11-12 01:19:35 +0300

    deploying

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ const router = jsonServer.router(path.join(__dirname, 'db/db.json'));
 const middlewares = jsonServer.defaults({ static: './dist' });
 const port = process.env.PORT || 3001;
 
+// eslint-disable-next-line import/no-extraneous-dependencies
+const cors = require('cors');
+
+const corsOptions = {
+  credentials: true,
+  origin: 'http://localhost:3001',
+  allowedHeaders: ['Content-Type'],
+  optionsSuccessStatus: 200,
+};
+server.use(cors(corsOptions));
+
 server.use(middlewares);
 server.use(router);
 server.listen(port);
