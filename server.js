f60ac61672be1db414867d1369352f12de849a21
mxassanov <mxassanov@bk.ru>
2021-11-12 01:54:22 +0300

    deploy application

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'src/db/db.json'));
 const middlewares = jsonServer.defaults({ static: './dist' });
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
--- src/services/http-common.js
+++ src/services/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:3001/',
+  baseURL: 'https://neuromodels.herokuapp.com/',
   headers: {
     'Content-Type': 'application/json',
   },
